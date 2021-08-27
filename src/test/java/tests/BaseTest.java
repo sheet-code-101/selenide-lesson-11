ed4d47c95567b1188dc762ae7bb33e0418f09ec6
Aleh <syllit95@gmail.com>
2021-08-27 10:02:53 +0300

    changes to BaseTest.java file in order to make allure work

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,11 +2,11 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attach;
 
 public class BaseTest {
 
@@ -16,7 +16,7 @@ public class BaseTest {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
@@ -28,7 +28,7 @@ public class BaseTest {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+
     }
 
 }
